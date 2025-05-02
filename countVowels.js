8e31398104e143f965d1485bd687c547622db65c
nvp85 <nvp@zhukov.al>
2025-05-02 14:19:22 -0500

    add one more test case for non string input

--- countVowels.js
+++ countVowels.js
@@ -1,5 +1,8 @@
 
 function countVowels(str) {
+    if (typeof str !== "string") {
+        throw new TypeError("Received input is not a string");
+    }
     const vowels = "aeiouAEIOU";
     return str.split("").filter(char => vowels.includes(char)).length;
 }
--- countVowels.test.js
+++ countVowels.test.js
@@ -9,8 +9,13 @@ describe('countVowels', () => {
         const res = countVowels("heLLO wORld");
         expect(res).toBe(3);
     });
-    test('should count vowels in an empty string', () => {
+    test('should return 0 if the string is empty', () => {
         const res = countVowels("");
         expect(res).toBe(0);
+    });
+
+    test('should throw a type error if the input is not a string', () => {
+        expect(() => countVowels(8)).toThrow("Received input is not a string");
+        expect(() => countVowels(8)).toThrow(TypeError);
     });
 })
